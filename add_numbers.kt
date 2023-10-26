77dc7d7377722fd14f1ca8dca2941cf7b5557cfe
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-10-26 22:10:47 +0900

    Update add_numbers.kt

--- add_numbers.kt
+++ add_numbers.kt
@@ -1,3 +1,4 @@
+##########################
 fun main() {
     val result = 1 + 1
     println("1 + 1 = $result")
@@ -8,3 +9,19 @@ fun main() {
 fun main() {
     println("Hello World!")
 }
+
+##########################
+
+fun main() {
+    val (n,m)= readLine()!!.split(" ").map{it.toInt()}
+    var sum =1
+    repeat(m){
+        sum*=n
+    }
+    println(sum)
+}
+#A,B=map(int,input().split())
+#print(A**B)
+
+
+##########################
