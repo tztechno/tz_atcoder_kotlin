3ec578fa479eb3ac0f20ff8b3bc837b4bf8e6415
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-01 21:48:39 +0900

    Create mid_letter.kt

---
+++ mid_letter.kt
@@ -0,0 +1,10 @@
+fun main() {
+    //print("Enter a string: ")
+    val input = readLine()
+
+    if (input != null) {
+        val n = input.length / 2
+        val ans = input[n]
+        println(ans)
+    }
+}
