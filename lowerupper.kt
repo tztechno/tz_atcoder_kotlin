190834eeaad4bb004c9e1086d70cb190e3e10a7e
stpeteishii <47345279+tztechno@users.noreply.github.com>
2024-01-11 22:14:11 +0900

    Update lowerupper.kt

--- lowerupper.kt
+++ lowerupper.kt
@@ -2,11 +2,51 @@
 
 ##################################################
 
+fun main(args: Array<String>) {
+    val s = readLine()!!
+    if(s.indices.all { if(it % 2 == 0) s[it] in ('a'..'z') else s[it] in ('A'..'Z') }) {
+        println("Yes")
+    } else {
+        println("No")
+    }
+}
 
 ##################################################
 
+fun main() {
+    val s = readLine()!!
+    var flag = false
+    if (!s[0].isLowerCase()) {
+        flag = true
+    }
+    
+    for (i in 1 until s.length) {
+        if (i % 2 == 0) {
+            if (!s[i].isLowerCase()) flag = true
+        } else {
+            if (!s[i].isUpperCase()) flag = true
+        }
+    }
+    
+    if (!flag) println("Yes") else println("No")
+}
 
 ##################################################
+
+import java.util.*
+import kotlin.math.*
+
+fun main(args: Array<String>){
+  val X = readLine()!!
+  val len = X.length
+  val oddIsLowerCase: Boolean = (0 until len step 2).all{X[it].isLowerCase()}
+  val evenIsUpperCase: Boolean = (1 until len step 2).all{X[it].isUpperCase()}
+  if(oddIsLowerCase && evenIsUpperCase)println("Yes") else println("No")
+  
+}
+
+##################################################
+
 import java.util.Scanner
 
 fun main() {
